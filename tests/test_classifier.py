062682633382f053268b6f7fa2e5563d74f960c1
Clement Vachet <1039042+clementsan@users.noreply.github.com>
2024-12-11 17:45:13 -0700

    Add test suites via pytest

---
+++ tests/test_classifier.py
@@ -0,0 +1,24 @@
+import pytest
+from classification.classifier import Classifier
+
+@pytest.fixture
+def setup_pipeline():
+    pipeline = Classifier()
+    pipeline.train_and_save()
+    return pipeline
+
+@pytest.fixture
+def requests():
+    return {
+        "features": [
+            [6.5, 3.0, 5.8, 2.2],
+            [6.1, 2.8, 4.7, 1.2]
+        ]
+    }
+
+@pytest.fixture
+def response():
+    return ['virginica', 'versicolor']
+
+def test_response(setup_pipeline, requests, response):
+    assert response == setup_pipeline.load_and_test(requests)['predictions']
