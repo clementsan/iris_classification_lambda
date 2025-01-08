ca264b805ee53904e39b2c466fcf00d50b1c249e
Clement Vachet <1039042+clementsan@users.noreply.github.com>
2025-01-08 16:49:22 -0700

    Improve code based on pylint and black suggestions

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -1,24 +1,31 @@
+"""
+Testing Classifier module
+"""
+
 import pytest
 from classification.classifier import Classifier
 
+
 @pytest.fixture
 def setup_pipeline():
+    """Setup classifier pipeline - training classifier and saving model"""
     pipeline = Classifier()
     pipeline.train_and_save()
     return pipeline
 
+
 @pytest.fixture
 def requests():
-    return {
-        "features": [
-            [6.5, 3.0, 5.8, 2.2],
-            [6.1, 2.8, 4.7, 1.2]
-        ]
-    }
+    """Example dataset"""
+    return {"features": [[6.5, 3.0, 5.8, 2.2], [6.1, 2.8, 4.7, 1.2]]}
+
 
 @pytest.fixture
 def response():
+    """Ground truth response from classifier"""
     return ["virginica", "versicolor"]
 
+
 def test_response(setup_pipeline, requests, response):
+    """Tests if classifier returns correct prediction"""
     assert response == setup_pipeline.load_and_test(requests["features"])["predictions"]
--- tests/test_lambda.py
+++ tests/test_lambda.py
@@ -1,33 +1,42 @@
+"""
+Testing Lambda handler
+"""
+
 import os
 import sys
-import pytest
 import json
+import pytest
+
+from lambda_function import lambda_handler
+
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, os.path.dirname(parent_dir))
 
-from lambda_function import lambda_handler
 
 @pytest.fixture
 def event():
-    json_event = {
-        "features": [[6.5, 3.0, 5.8, 2.2], [6.1, 2.8, 4.7, 1.2]]
-    }
+    """Example json event"""
+    json_event = {"features": [[6.5, 3.0, 5.8, 2.2], [6.1, 2.8, 4.7, 1.2]]}
     return json_event
+
 
 @pytest.fixture
 def context():
+    """Example context"""
     return None
+
 
 @pytest.fixture
 def response_prediction():
+    """Ground truth - API response"""
     return ["virginica", "versicolor"]
 
 
 def test_lambda_handler(event, context, response_prediction):
+    """Tests lambda handler"""
     lambda_response = lambda_handler(event, context)
 
     assert lambda_response["statusCode"] == 200
     assert json.loads(lambda_response["body"])["predictions"] == response_prediction
-
